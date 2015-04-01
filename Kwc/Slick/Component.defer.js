d4d403c8ef1daa0ad7a18604395e4cc68e3038de
Niko Sams <ns@vivid-planet.com>
2015-04-01 09:55:02 +0200

    Initialize cloned img components

--- Kwc/Slick/Component.defer.js
+++ Kwc/Slick/Component.defer.js
@@ -2,5 +2,8 @@ Kwf.onJElementReady('.kwcSlick', function(el, config) {
     var slickWrapper = el.find('.listWrapper');
     if (slickWrapper.length) {
         slickWrapper.slick(config.slickConfig);
+
+        //to initialize cloned elements
+        Kwf.callOnContentReady(slickWrapper, { action: 'render' });
     }
 });
