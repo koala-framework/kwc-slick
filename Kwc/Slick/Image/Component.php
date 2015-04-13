919da9da2971dc24f4c6f8c521bc23df9d575a37
Daniel Bacher <db@vivid-planet.com>
2015-04-13 14:07:08 +0200

    Bugfix type error in code

--- Kwc/Slick/Image/Component.php
+++ Kwc/Slick/Image/Component.php
@@ -4,7 +4,6 @@ class Kwc_Slick_Image_Component extends Kwc_Basic_Image_Component
     public static function getSettings()
     {
         $ret = parent::getSettings();
-        $ret['generators']['child']['component']
         return $ret;
     }
 }
